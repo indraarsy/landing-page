301584c751756f6de350b32614d21b958e666474
indraarsy <userindra29@gmail.com>
2020-11-02 23:14:16 +0700

    skills blm selesai

---
+++ components/skills.js
@@ -0,0 +1,13 @@
+export default function Skills() {
+    return(
+        <div className="container flex mx-auto h-screen">
+            {/* Header */}
+            <div className="my-auto">
+                <span className="font-serif md:text-5xl text-3xl font-bold text-gray-700">Skills</span>
+                <div className="grid-cols-3">
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -3,102 +3,106 @@ import Nav from '../components/nav'
 import Home from '../components/home'
 import Particles from "react-tsparticles";
 import { useMediaQuery } from 'react-responsive'
+import Skills from '../components/skills';
 
 export default function IndexPage() {
   const isTabletOrMobileDevice = useMediaQuery({
     query: '(max-device-width: 1224px)'
   })
   return (
-    <div className={(isTabletOrMobileDevice == true) ? 'mobile ' : 'h-screen ' + "lg:container lg:mx-auto relative"}>
-      <Head>
-        <title>Indra Arsy Kaloka</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-      </Head>
-      <Particles
-        id="tsparticles"
-        className="absolute mobile w-full bg-opacity-25"
-        options={{
-          background: {
-            color: {
-              value: "#fff",
+    <div>
+      <div className={(isTabletOrMobileDevice == true) ? 'mobile ' : 'h-screen ' + "lg:container lg:mx-auto relative"}>
+        <Head>
+          <title>Indra Arsy Kaloka</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
+        <Particles
+          id="tsparticles"
+          className="absolute mobile w-full bg-opacity-25"
+          options={{
+            background: {
+              color: {
+                value: "#fff",
+              },
             },
-          },
-          fpsLimit: 60,
-          interactivity: {
-            detectsOn: "window",
-            events: {
-              onClick: {
+            fpsLimit: 60,
+            interactivity: {
+              detectsOn: "window",
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: "push",
+                },
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                resize: true,
+              },
+              modes: {
+                bubble: {
+                  distance: 400,
+                  duration: 2,
+                  opacity: 0.8,
+                  size: 40,
+                },
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 100,
+                  duration: 0.4,
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: "#000",
+              },
+              links: {
+                color: "#A9A9A9",
+                distance: 150,
                 enable: true,
-                mode: "push",
+                opacity: 0.5,
+                width: 1,
               },
-              onHover: {
+              collisions: {
                 enable: true,
-                mode: "repulse",
               },
-              resize: true,
-            },
-            modes: {
-              bubble: {
-                distance: 400,
-                duration: 2,
-                opacity: 0.8,
-                size: 40,
-              },
-              push: {
-                quantity: 4,
-              },
-              repulse: {
-                distance: 100,
-                duration: 0.4,
-              },
-            },
-          },
-          particles: {
-            color: {
-              value: "#000",
-            },
-            links: {
-              color: "#A9A9A9",
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            collisions: {
-              enable: true,
-            },
-            move: {
-              direction: "none",
-              enable: true,
-              outMode: "bounce",
-              random: false,
-              speed: 4,
-              straight: false,
-            },
-            number: {
-              density: {
+              move: {
+                direction: "none",
                 enable: true,
-                value_area: 800,
+                outMode: "bounce",
+                random: false,
+                speed: 4,
+                straight: false,
               },
-              value: 25,
+              number: {
+                density: {
+                  enable: true,
+                  value_area: 800,
+                },
+                value: 25,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "circle"
+              },
+              size: {
+                random: true,
+                value: 3,
+              },
             },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "circle"
-            },
-            size: {
-              random: true,
-              value: 3,
-            },
-          },
-          detectRetina: true,
-        }}
-      />
-      <Nav />
-      <Home />
+            detectRetina: true,
+          }}
+        />
+        <Nav />
+        <Home />
+      </div>
+      <Skills />
     </div >
   )
 }
